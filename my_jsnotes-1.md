f1179395d99bbb1d607893b461fc4e7aec675922
jadashi97 <njadauniversal23@yahoo.com>
2022-02-28 19:01:05 -0600

    Please enter the commit message for your changes. Lines starting initial commit docs(feat): index.html docs(feat): my_jsnotes-1.md
    docs(feat): project1.js
    
    worked on a few sytanx in javascrip
    
    docs(syntax):project2.js
    
    worked on a few beginner js things

---
+++ my_jsnotes-1.md
@@ -0,0 +1,18 @@
+# notes on basics and syntax of javascript 
+/*
+used for multiline commenting
+/*
+
+/* Data Types:
+undefined, null, boolean, string, symbol, number, and object
+*/
+// used for one line commenting
+
+var = used to set a variable
+let = also used to set a variable
+const = used to set a  const that cant be changed
+;(semicolon) = used after every line of code
+console.log() = used to run code
+
+myName = use that format used to name a variable
+ 
---
+++ project1.js
@@ -0,0 +1,22 @@
+// Eg 1.
+
+var a;
+a = 2
+var b;
+b = 5
+var c;
+c = "I am old enough"
+
+
+// Eg 2
+
+let myFirstName = "Toni Rose ";
+let MyLastName = " Roncales ";
+const LOCATION = " Bloomington "; 
+let school = " Normandale CC "
+
+console.log(" My name is "+ myFirstName + MyLastName + 
+            " I live in " + LOCATION + 
+            " with my sweetHeart Daniel" + 
+            " And we go to " + school + 
+            " together, He deosnt know it But I love him so much to the MOON and back. Even if he scared the crap out of me this morning. I got KILIG when he showed up and surprised me ")
