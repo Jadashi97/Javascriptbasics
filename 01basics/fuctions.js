dbb55446dc80649edb7a98e63c543966734e8cb3
jadashi97 <njadauniversal23@yahoo.com>
2022-03-03 14:45:11 -0600

    docs:(feat):worked on more javascript basics plus organized some of my files and notes

---
+++ 01basics/fuctions.js
@@ -0,0 +1,40 @@
+//Working with Operators
+/*
+// eg 1. calculations
+//D = (L - S)/L * 100
+
+var listingPrice = 286;
+var sellingPrice = 437;
+
+var discountPrice = (listingPrice - sellingPrice) /listingPrice * 100;
+console.log(discountPrice);
+
+// eg2. regular
+function myName(){
+    console.log("Nyarji Daniel Jada")
+
+}
+myName();
+
+//eg3. functions with arguments 
+function myFam(names){
+    console.log(names);
+}
+myFam("lemi");
+
+//eg4a. global vs local functios 
+var myBro = "lemi";
+function myBrothers(){
+    myBro = "Toni";
+    console.log();
+}
+console.log(myBro);
+
+//eg4b.
+let myBro = "lemi";
+function myBro() {
+    const myBro = "Tom";
+    return myBro;
+}
+console.log(myBro);
+**/
