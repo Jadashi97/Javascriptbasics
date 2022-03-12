76e21e8c3f1bfba5cf6654d22cfda019b4d716a8
jadashi97 <njadauniversal23@yahoo.com>
2022-03-12 10:52:41 -0600

    docs:(feat):worked on some funcions

---
+++ HiteshTuts/01funct.js
@@ -0,0 +1,32 @@
+/*
+// using the function key and incorporating the (switch/ case)
+function userRole(name, role){
+switch (role) {
+    case 'Admin':
+    return `${name} access permitted and you can change stuff.`;
+
+    case "user":
+    return `${name} please get permission first`;
+    
+    case "guest":
+        return `${name} do you have a guest passs? `;
+}
+}
+console.log(userRole("lokose", "guest"))
+*/
+// incoorporate the function Expression
+
+var userRole = function(name, role) {
+    switch (role) {
+        case 'Admin':
+        return `${name} access permitted and you can change stuff.`;
+    
+        case "user":
+        return `${name} please get permission first`;
+        
+        case "guest":
+            return `${name} do you have a guest passs? `;
+    }
+
+}
+console.log(userRole("lokose", "user"))
