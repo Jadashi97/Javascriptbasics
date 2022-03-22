5bd903c1ef00248bb5c4c54034b2fc987fade9c7
jadashi97 <njadauniversal23@yahoo.com>
2022-03-21 20:34:11 -0500

    docs:(feat):work on objects in javascript

---
+++ codecamp/objects.js
@@ -0,0 +1,46 @@
+//bulding javascript objects
+/*
+    eg1. 
+const cat = {
+name: "whiskers",
+legs: 4,
+tails: 1,
+enenemies: ["water", "Dogs"]
+};
+
+EX 1 
+const myDog = {
+name: "papichow",
+legs: 4,
+tails:1,
+friends: ["babiny", "otot"]
+};
+
+//Dot Notation to acces object properties
+//eg.2
+const myObj = {
+    prop1 : "val 1",
+    prop2 : "val 2",
+}
+console.log(myObj.prop1);
+
+//EX.2
+const testObj = {
+    hatValue : "hat",
+    shirtValue : "shirt",
+};
+console.log(testObj.shirtValue);
+*/
+// accesing objects with bracket Notation
+const myFam = {
+    "first born" :"Tom",
+    "second born" :"lokose",
+    "third born": "wani",
+    "fourth born" : "Lemi",
+
+};
+myFam["first born"];
+
+// accessing object properties with variables
+
+
