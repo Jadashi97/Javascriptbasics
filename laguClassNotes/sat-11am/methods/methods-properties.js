703a84da1ef41c35ec437b19cadb48a7a2c17f43
jadashi97 <njadauniversal23@yahoo.com>
2022-03-29 22:10:01 -0500

    feat:(docs):work methods and properties within strings

---
+++ laguClassNotes/sat-11am/methods/methods-properties.js
@@ -0,0 +1,31 @@
+const fam = "I have a big family"
+
+// Methods
+
+// console.log(fam.charAt(0));
+// returns a character at a sepcified index
+
+console.log(fam.charCodeAt(5));
+// returns an integer between 0 and 65535 representing the UTF-16 code unit at the given index.
+
+fam.concat()
+// Returns a string that contains the concatenation of two or more strings
+
+.endsWith()
+// determines whether a string ends with the characters of a specified string, returning true or false as appropriate.
+
+fam.includes()
+// performs a case-sensitive search to determine whether one string may be found within another string, returning true or false as appropriate.
+
+fam.indexOf()
+// returns the index of the first occurrence of the specified substring.
+
+fam.lastIndexOf()
+// Returns the last occurrence of a substring in the string.
+
+fam.localeCompare()
+// Determines whether two strings are equivalent in the current or specified locale.
+
+fam.match()
+// method retrieves the result of matching a string against a regular expression.
+
