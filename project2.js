5730df2e32577e9da472a8e6bdff32734c17d7ed
jadashi97 <njadauniversal23@yahoo.com>
2022-04-08 09:41:41 -0500

    feat:(docs):solving some file issues

---
+++ project2.js
@@ -0,0 +1,10 @@
+let myFirstName = "Toni Rose ";
+let MyLastName = " Roncales ";
+const LOCATION = " Bloomington "; 
+let school = " Normandale CC "
+
+console.log(" My name is "+ myFirstName + MyLastName + 
+            " I live in " + LOCATION + 
+            " with my sweetHeart Daniel" + 
+            " And we go to " + school + 
+            " together, He deosnt know it But I love him so much to the MOON and back. Even if he scared the crap out of me this morning. I got KILIG when he showed up and surprised me ")
