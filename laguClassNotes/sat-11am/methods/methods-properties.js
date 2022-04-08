e7ffcfbc2d88d03090683cca83970f15ecac68e9
jadashi97 <njadauniversal23@yahoo.com>
2022-04-08 11:42:30 -0500

    feat:(docs):add a few string methods

--- laguClassNotes/sat-11am/methods/methods-properties.js
+++ laguClassNotes/sat-11am/methods/methods-properties.js
@@ -1,6 +1,6 @@
 const fam = "I have a big family"
 
-// Methods
+// // Methods
 
 // console.log(fam.charAt(0));
 // returns a character at a sepcified index
@@ -8,24 +8,86 @@ const fam = "I have a big family"
 console.log(fam.charCodeAt(5));
 // returns an integer between 0 and 65535 representing the UTF-16 code unit at the given index.
 
-fam.concat()
+fam.concat();
 // Returns a string that contains the concatenation of two or more strings
 
-.endsWith()
+fam.endsWith();
 // determines whether a string ends with the characters of a specified string, returning true or false as appropriate.
 
-fam.includes()
+fam.includes();
 // performs a case-sensitive search to determine whether one string may be found within another string, returning true or false as appropriate.
 
-fam.indexOf()
+fam.indexOf();
 // returns the index of the first occurrence of the specified substring.
 
-fam.lastIndexOf()
+fam.lastIndexOf();
 // Returns the last occurrence of a substring in the string.
 
-fam.localeCompare()
+fam.localeCompare();
 // Determines whether two strings are equivalent in the current or specified locale.
 
-fam.match()
+fam.match();
 // method retrieves the result of matching a string against a regular expression.
 
+fam.matchAll();
+// Matches a string with a regular expression, and returns an iterable of matches containing the results of that search.
+
+fam.padEnd();
+// Pads the current string with a given string (possibly repeated) so that the resulting string reaches a given length. The padding is applied from the end (right) of the current string.
+
+fam.padStart();
+// Pads the current string with a given string (possibly repeated) so that the resulting string reaches a given length. The padding is applied from the start (left) of the current string.
+
+fam.repeat();
+// Returns a String value that is made from count copies appended together. If count is 0, the empty string is returned.
+
+fam.replace();
+// Replaces text in a string, using a regular expression or search string.
+
+fam.search();
+// Finds the first substring match in a regular expression search.
+
+fam.slice();
+// Returns a section of a string.
+
+fam.split();
+// Split a string into substrings using the specified separator and return them as an array.
+
+fam.startsWith();
+// determines whether a string begins with the characters of a specified string, returning true or false as appropriate.
+
+fam.substring();
+// Returns the substring at the specified location within a String object.
+
+fam.toLocaleLowerCase();
+// Converts all alphabetic characters to lowercase taking into account the host environment's current locale.
+
+fam.toLocaleUpperCase();
+// Returns a string where all alphabetic characters have been converted to uppercase taking into account the host environment's current locale.
+
+fam.toLowerCase();
+// Converts all the alphabetic characters in a string to lowercase.
+
+fam.toUpperCase();
+// Converts all the alphabetic characters in a string to uppercase.
+
+fam.toString();
+// Returns a string representation of a string.
+
+fam.trim();
+// Removes the leading and trailing white space and line terminator characters from a string.
+
+fam.trimEnd();
+// Removes the trailing white space and line terminator characters from a string.
+
+fam.trimStart();
+// Removes the leading white space and line terminator characters from a string.
+
+fam.valueOf();
+// Returns the primitive value of the specified object.
+
+fam.normalize();
+// returns the Unicode Normalization Form of the string.
+
+
+// // Properties
