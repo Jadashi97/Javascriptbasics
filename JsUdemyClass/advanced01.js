d39701660d2f5b209c8193a03e1757e420699750
jadashi97 <njadauniversal23@yahoo.com>
2022-04-29 21:18:45 -0500

    (feat):uncomment some of the codes to help with easy revision

--- JsUdemyClass/advanced01.js
+++ JsUdemyClass/advanced01.js
@@ -4,45 +4,46 @@
 
 // concepts behind a calculator and Higher order functions(passing a function into as arguments)
 
-// function add(num1, num2){
-//     return num1 + num2
-// };
+function add(num1, num2){
+    return num1 + num2
+};
 
-// function multiply(num1, num2){
-//     return num1 * num2
-// };
+function multiply(num1, num2){
+    return num1 * num2
+};
 
-// function subtract(num1, num2){
-//     return num1 - num2
-// };
+function subtract(num1, num2){
+    return num1 - num2
+};
 
-// function divide(num1, num2){
-//     return num1 / num2
-// };
-
-
-// function calculator(num1, num2, operator){
-//     return operator(num1, num2)
-// };
-
-// console.log(calculator(6,3,divide));
+function divide(num1, num2){
+    return num1 / num2
+};
 
 
-// // working with constructor functions and (new and this) keywords
+function calculator(num1, num2, operator){
+    return operator(num1, num2)
+};
 
-// function HouseKeeper(name, yearWorking, workPermit){
-//     this.name = name;
-//     this.workPermit = workPermit;
-//     this.yearWorking = yearWorking;
-// };
+console.log(calculator(6,3,divide));
 
-// var houseKeeper1 = new HouseKeeper("lokose",true, "10 years");
-// console.log(houseKeeper1);
+
+// working with constructor functions and (new and this) keywords
+
+function HouseKeeper(name, yearWorking, workPermit){
+    this.name = name;
+    this.workPermit = workPermit;
+    this.yearWorking = yearWorking;
+};
+
+var houseKeeper1 = new HouseKeeper("lokose",true, "10 years");
+console.log(houseKeeper1);
 
 // the logic behind callback functions
 
 function anotherAddEventListener(typeOfEvent, callback){
 
+    // this is an object
     var eventThatHappend = {
         eventType: "keyPress",
         key: "p",
