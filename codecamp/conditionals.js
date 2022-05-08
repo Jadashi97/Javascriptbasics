93629fc3a71b1650d64ca105a8cfda088a0cf16e
jadashi97 <njadauniversal23@yahoo.com>
2022-05-08 18:42:25 -0500

    (feat): internalize loop concepts

--- codecamp/conditionals.js
+++ codecamp/conditionals.js
@@ -1,91 +1,93 @@
 
-// // #1 if statement
-// function test (myFam){
-//     if (myFam) {
-//         return "It was true";
-//     }
-//     return "It was false ";
-// }
-// console.log(test(true));
-// //console.log(test(true));
+// #1 if statement
+function test (myFam){
+    if (myFam) {
+        return "It was true";
+    }
+    return "It was false ";
+}
+console.log(test(true));
+//console.log(test(true));
 
-// // #2 with equality operators
-// function equalityTest(myVal){
-//     if (myVal == 10){
-//         return "Equal";
-//     }
-//     return "Not Equal";
-// }
-// console.log(equalityTest(10));
+// #2 with equality operators
+function equalityTest(myVal){
+    if (myVal == 10){
+        return "Equal";
+    }
+    return "Not Equal";
+}
+console.log(equalityTest(10));
 
-// // #2 
-// function findingCars(camry) {
-//     if (camry == "red"){
-//         return "yes thats mine";
-//     }
-//     else "nahh thats not mine";
-// }
-// console.log(findingCars("red"));
+// #2 
+function findingCars(camry) {
+    if (camry == "red"){
+        return "yes thats mine";
+    }
+    else "nahh thats not mine";
+}
+console.log(findingCars("red"));
 
-// // #3 using infor to pusrchase something online
+// #3 using infor to pusrchase something online
 
-// // let userLoggedIn = true;
-// // let verifiedEmail = false;
-// // let cardInfor = false;
-
-
-// if (userLoggedIn){
-//     console.log("please continue to the next step");
-//     if (verifiedEmail){
-//         console.log("zoot keeep going to the next step");
-//         if (cardInfor){
-//             console.log("please pay up here!! ")
-//         }
-//     }
-// }
-
-// // #4 using infor to pusrchase something online
 // let userLoggedIn = true;
-// let verifiedEmail = true;
-// let cardInfor = true;
+// let verifiedEmail = false;
+// let cardInfor = false;
 
-// if (userLoggedIn && verifiedEmail && cardInfor){
-//     console.log("awesome, You are allowed to buy the goodies");
+
+if (userLoggedIn){
+    console.log("please continue to the next step");
+    if (verifiedEmail){
+        console.log("zoot keeep going to the next step");
+        if (cardInfor){
+            console.log("please pay up here!! ")
+        }
+    }
+}
+
+// #4 using infor to pusrchase something online
+let userLoggedIn = true;
+let verifiedEmail = true;
+let cardInfor = true;
+
+if (userLoggedIn && verifiedEmail && cardInfor){
+    console.log("awesome, You are allowed to buy the goodies");
     
-// } else{
-//     console.log("please one of your infor is not right!!!!");
-// }
+} else{
+    console.log("please one of your infor is not right!!!!");
+}
 
 
 // iterating js for loops
 
 
-// for(let i = 1; i < 6; i++){
-//   myArray.push(i);
-// }
+for(let i = 1; i < 6; i++){
+  myArray.push(i);
+}
 
-// //iterating odd numbers with a for loop
+//iterating odd numbers with a for loop
 
-// for(let i = 1; i < 10; i+=2){
-//   myArray.push(i);
-// };
+for(let i = 1; i < 10; i+=2){
+  myArray.push(i);
+};
 
-//counting backwards with a for loop
-// const myArray = [];
+// counting backwards with a for loop
+const myArray = [];
 
-// for(let i = 9; i > 0; i-=2){
-//     console.log(myArray.push(i));
-// }
+for(let i = 9; i > 0; i-=2){
+    console.log(myArray.push(i));
+}
 
 //iterating thru an array with a for loop
 
 const theArr = [2, 3, 4, 5, 6];
 
-const total = [];
-//this iterates the numbers in the array untill it gets to the index below that is needed
+let total = 0;
+//this iterates the numbers in the array until it gets to the index below that is needed
 
 for(let i = 0; i < theArr.length; i++){
-   //so this adds all the numbers in the array      finish this
 
-  console.log(total.push(total += theArr.length));
+  //so this adds all the numbers in the array to give a total of 20
+  console.log(total += theArr[i]);
 }
+
+
