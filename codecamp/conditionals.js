c08109e8a6cd3410b8ab2d2afa486f9948a9b545
jadashi97 <njadauniversal23@yahoo.com>
2022-05-07 23:20:32 -0500

    Changes to be committed: 	modified:   conditionals.js 	modified:   ifelsestate.js 	modified:   objects.js  Changes not staged for commit: 	modified:   ../.DS_Store 	modified:   ../JsUdemyClass/Intermediate01.js 	modified:   ../JsUdemyClass/advanced01.js  Untracked files: i	../laguClassNotes/js-function-session-note-main/

--- codecamp/conditionals.js
+++ codecamp/conditionals.js
@@ -11,7 +11,7 @@ console.log(test(true));
 
 // #2 with equality operators
 function equalityTest(myVal){
-    if (myVal ===10){
+    if (myVal == 10){
         return "Equal";
     }
     return "Not Equal";
@@ -20,7 +20,7 @@ console.log(equalityTest(10));
 
 // #2 
 function findingCars(camry) {
-    if (camry === "red"){
+    if (camry == "red"){
         return "yes thats mine";
     }
     else "nahh thats not mine";
@@ -45,6 +45,10 @@ if (userLoggedIn){
 }
 
 // #4 using infor to pusrchase something online
+let userLoggedIn = true;
+let verifiedEmail = true;
+let cardInfor = true;
+
 if (userLoggedIn && verifiedEmail && cardInfor){
     console.log("awesome, You are allowed to buy the goodies");
     
@@ -52,51 +56,3 @@ if (userLoggedIn && verifiedEmail && cardInfor){
     console.log("please one of your infor is not right!!!!");
 }
 
-//checking the logic behind while loops 
-
-let i = 5;
-// backwards 
-while(i > 0){
-    console.log(i);
-    i--;
-}
-var n = 0
-// forwards
-while(n < 5){
-    console.log(n);
-    n++;
-}
-
-
-//checking the logic behind for loops
-//backwards
-for(let i = 10; i > 0; i -= 2){
-    console.log(i);
-}
-// forwards
-for(let i = 0; i < 10; i += 2){
-    console.log(i);
-}
-
-// iterating thru an array and adding the total to the index of the array
-
-const myArr = [2, 3, 4, 5, 6];
-let total = 0;
-
-for(let i = 0; i < myArr.length; i ++ ){
-    console.log(total += myArr[i]);
-};
-
-//Nesting for loops technically working with an array within another array <finish this>
-
-function multiplyAll(arr) {
-    let product = 1;
-    for(let i = 0; i < arr.length; i++){
-        for(let j = 0; j < arr.length[i]; j++){ 
-            console.log(arr[i][j]);
-        }
-    }
-    return product;
-}
-  
-multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
