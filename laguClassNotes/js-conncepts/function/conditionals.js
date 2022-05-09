710ed37734673b5b5d0046a8d1bf0c77e20c8488
jadashi97 <njadauniversal23@yahoo.com>
2022-05-09 00:01:08 -0500

    (feat): internalize loop concepts

--- laguClassNotes/js-conncepts/function/conditionals.js
+++ laguClassNotes/js-conncepts/function/conditionals.js
@@ -1,58 +1,58 @@
-// Data types - strings, number, objects, arrays, date
-const { log } = console; // object destructuring
+// // Data types - strings, number, objects, arrays, date
+// const { log } = console; // object destructuring
 
-const num = 3;
-let age = "3";
-// ==
-log(num == age);
+// const num = 3;
+// let age = "3";
+// // ==
+// log(num == age);
 
-// ===
-log(num === age); // exact/strict
+// // ===
+// log(num === age); // exact/strict
 
-const userNames = ["Justin", "john", "Mama", "Aman", "Emma", "Juma"];
+// const userNames = ["Justin", "john", "Mama", "Aman", "Emma", "Juma"];
 
-const jUserNames = [];
+// const jUserNames = [];
 
-for (let i = 0; i < userNames.length; i++) {
-	// making decision
-	if (userNames[i].toUpperCase().startsWith("J")) {
-		// do something
-		jUserNames.push(userNames[i]);
-	}
-}
+// for (let i = 0; i < userNames.length; i++) {
+// 	// making decision
+// 	if (userNames[i].toUpperCase().startsWith("J")) {
+// 		// do something
+// 		jUserNames.push(userNames[i]);
+// 	}
+// }
 
-log(`There are ${jUserNames.length} number of users with name starting with J`);
+// log(`There are ${jUserNames.length} number of users with name starting with J`);
 
-// Conditionals
+// // Conditionals
 
-/*
-- Conditionals are decissional -> directions
-- expression that depends on its evaluation value to true/false (booleans)
-*/
+// /*
+// - Conditionals are decissional -> directions
+// - expression that depends on its evaluation value to true/false (booleans)
+// */
 
-const itsRaining = true;
+// const itsRaining = true;
 
-if (itsRaining) {
-	// carry umbrella
-	// log("I am carrying umbrella because it's raining");
-} else {
-	// walk without umbrella
-	// log("No need to carry umbrella, it's not raining");
-}
+// if (itsRaining) {
+// 	// carry umbrella
+// 	// log("I am carrying umbrella because it's raining");
+// } else {
+// 	// walk without umbrella
+// 	// log("No need to carry umbrella, it's not raining");
+// }
 
-// Traffic
+// // Traffic
 
-let trafficLight = "yellow";
+// let trafficLight = "yellow";
 
-if (trafficLight === "red") {
-	log("stop");
-} else if (trafficLight === "green") {
-	log("go");
-} else if (trafficLight === "yellow") {
-	log("go with caution");
-} else {
-	log("drive");
-}
+// if (trafficLight === "red") {
+// 	log("stop");
+// } else if (trafficLight === "green") {
+// 	log("go");
+// } else if (trafficLight === "yellow") {
+// 	log("go with caution");
+// } else {
+// 	log("drive");
+// }
 
 // Other conditional condition
 // Ternary operation
@@ -63,13 +63,13 @@ light === "red" ? log("Stop!") : log("Go!"); // assignment: write else if versio
 let light = "red";
 
 if (light === "red"){
-    log("Stop");
+    console.log("Stop");
 }else{
-    log("Go!");
+    console.log("Go!");
 }
 
-// nullcoalescence
-itsRaining && log("carry umbrella");
-!itsRaining && log("leave umbrella");
+// // nullcoalescence
+// itsRaining && log("carry umbrella");
+// !itsRaining && log("leave umbrella");
 
-// Switch statement
+// // Switch statement
