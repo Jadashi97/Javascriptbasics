e6c3fe8ba11aca9e84e1a5d1cb35b11facada724
jadashi97 <njadauniversal23@yahoo.com>
2022-05-13 18:35:50 -0500

    (feat): finish assignment on terney operators

--- laguClassNotes/js-conncepts/function/conditionals.js
+++ laguClassNotes/js-conncepts/function/conditionals.js
@@ -1,75 +1,81 @@
 // // Data types - strings, number, objects, arrays, date
-// const { log } = console; // object destructuring
+const { log } = console; // object destructuring
 
-// const num = 3;
-// let age = "3";
-// // ==
-// log(num == age);
+const num = 3;
+let age = "3";
+// ==
+log(num == age);
 
-// // ===
-// log(num === age); // exact/strict
+// ===
+log(num === age); // exact/strict
 
-// const userNames = ["Justin", "john", "Mama", "Aman", "Emma", "Juma"];
+const userNames = ["Justin", "john", "Mama", "Aman", "Emma", "Juma"];
 
-// const jUserNames = [];
+const jUserNames = [];
 
-// for (let i = 0; i < userNames.length; i++) {
-// 	// making decision
-// 	if (userNames[i].toUpperCase().startsWith("J")) {
-// 		// do something
-// 		jUserNames.push(userNames[i]);
-// 	}
-// }
+for (let i = 0; i < userNames.length; i++) {
+	// making decision
+	if (userNames[i].toUpperCase().startsWith("J")) {
+		// do something
+		jUserNames.push(userNames[i]);
+	}
+}
 
-// log(`There are ${jUserNames.length} number of users with name starting with J`);
+log(`There are ${jUserNames.length} number of users with name starting with J`);
 
-// // Conditionals
+// Conditionals
 
-// /*
-// - Conditionals are decissional -> directions
-// - expression that depends on its evaluation value to true/false (booleans)
-// */
+/*
+- Conditionals are decisional -> directions
+- expression that depends on its evaluation value to true/false (booleans)
+*/
 
-// const itsRaining = true;
+const itsRaining = true;
 
-// if (itsRaining) {
-// 	// carry umbrella
-// 	// log("I am carrying umbrella because it's raining");
-// } else {
-// 	// walk without umbrella
-// 	// log("No need to carry umbrella, it's not raining");
-// }
+if (itsRaining) {
+	// carry umbrella
+	// log("I am carrying umbrella because it's raining");
+} else {
+	// walk without umbrella
+	// log("No need to carry umbrella, it's not raining");
+}
 
-// // Traffic
+// convert the following to ternery operator
 
-// let trafficLight = "yellow";
+let trafficLight = "red";
 
-// if (trafficLight === "red") {
-// 	log("stop");
-// } else if (trafficLight === "green") {
-// 	log("go");
-// } else if (trafficLight === "yellow") {
-// 	log("go with caution");
-// } else {
-// 	log("drive");
-// }
+if (trafficLight === "red") {
+	console.log("stop");
+} else if (trafficLight === "green") {
+	console.log("go");
+} else if (trafficLight === "yellow") {
+	console.log("go with caution");
+} else {
+	console.log("drive");
+}
+
+// Ternary version of the above
+trafficLight === "red" ? log("stop!") : "green"? log("green"): "yellow"? log("go with caution"): log("drive")
 
 // Other conditional condition
 // Ternary operation
-light === "red" ? log("Stop!") : log("Go!"); // assignment: write else if version for this.
+
+trafficLight === "red" ? log("Stop!") : log("Go!"); // assignment: write else if version for this.
 
 // the if else version of the ternary operation above
 
-let light = "red";
+let light = "purple";
 
 if (light === "red"){
     console.log("Stop");
-}else{
+}else if(light != "red"){
     console.log("Go!");
+}else{ //not entirely sure if i need this last else since its not referenced in the ternery opearator
+console.log("Go");
 }
 
-// // nullcoalescence
-// itsRaining && log("carry umbrella");
-// !itsRaining && log("leave umbrella");
+// nullcoalescence
+itsRaining && log("carry umbrella");
+!itsRaining && log("leave umbrella");
 
 // // Switch statement
