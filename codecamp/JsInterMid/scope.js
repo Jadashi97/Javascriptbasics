73338f9e7fa3863df1604bf3b75a2d5e2842f740
jadashi97 <njadauniversal23@yahoo.com>
2022-06-06 20:51:32 -0500

    (feat):more revision on vanillla js

---
+++ codecamp/JsInterMid/scope.js
@@ -0,0 +1,44 @@
+// //compare let and var keys
+// function checkScope() {
+//     let i = 'function scope';
+//     if (true) {
+//        let i = 'block scope';
+//       console.log('Block scope i is: ', i);
+//     }
+//     console.log('Function scope i is: ', i);
+//     return i;
+// };
+
+// console.log(checkScope());
+
+// //mutate an array declared with const
+// const s = [5, 7, 2];
+// function editInPlace() {
+//   // Only change code below this line
+//   console.log(s[0] = 2,
+//             s[1] = 5,
+//                 s[2] = 7);
+//   // Using s = [2, 5, 7] would be invalid
+
+//   // Only change code above this line
+// }
+// editInPlace();
+
+//Prevent object mutation
+function freezeObj() {
+    const MATH_CONSTANTS = {
+      PI: 3.14
+    };
+    // Only change code below this line
+    Object.freeze(MATH_CONSTANTS);
+  
+    // Only change code above this line
+    try {
+      MATH_CONSTANTS.PI = 99;
+    } catch(ex) {
+      console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+  }
+  const PI = freezeObj();
+  console.log(PI);
