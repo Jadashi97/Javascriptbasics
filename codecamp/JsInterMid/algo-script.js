991ce19895f16b179eaf9b51b5f5a97be7ca6834
jadashi97 <njadauniversal23@yahoo.com>
2022-06-13 21:49:54 -0500

    (feat):revise more basic js concepts(methods, arrays, properties)

---
+++ codecamp/JsInterMid/algo-script.js
@@ -0,0 +1,14 @@
+// Return Largest Numbers in Arrays
+// finish this QA
+
+function largestOfFour(arr) {
+    let maxNum = [];
+
+    for(let i = 0; i < arr[i]; i++){
+        
+        // if(sum(arr[i]) );
+    };
+    // return arr;
+}
+  
+console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
--- codecamp/JsInterMid/basicPrac.js
+++ codecamp/JsInterMid/basicPrac.js
@@ -1,37 +1,57 @@
-// //reverse a string
+//reverse a string
 
-// function reverseString(str) {
-//     return str
-//     .split("") //this splits it
-//     .reverse()//reverses it 
-//     .join(""); //joins it back
-// }
+function reverseString(str) {
+    return str
+    .split("") //this splits it
+    .reverse()//reverses it 
+    .join(""); //joins it back
+}
   
-// // console.log(reverseString("hello"));
+// console.log(reverseString("hello"));
 
 
-// //FACTORIALIZE A NUMBER
-// function factorialize(num) {
-//     if(num === 0){ //check this first
-//       return 1; //return this when true 
-//     }else{
-//         return num * factorialize(num - 1);
-//     }
-// }
+//FACTORIALIZE A NUMBER
+function factorialize(num) {
+    if(num === 0){ //check this first
+      return 1; //return this when true 
+    }else{
+        return num * factorialize(num - 1);
+    }
+}
   
-// console.log(factorialize(5));
+console.log(factorialize(5));
 
-// // find longest word in a string
+// find longest word in a string
 
-// function findLongestWordLength(str) {
-//     let x = str.split(" "); //set a variable to keep the split words
-//     let maxLength = 0;
-//     for(let i = 0; i < x.length; i++){ //loop thru the  words
-//         if(x[i].length > maxLength){ //find the word with the longest length
-//            maxLength =  x[i].length;
-//         }
-//     }
-//     return maxLength;
-// }
+function findLongestWordLength(str) {
+    let x = str.split(" "); //set a variable to keep the split words
+    let maxLength = 0;
+    for(let i = 0; i < x.length; i++){ //loop thru the  words
+        if(x[i].length > maxLength){ //find the word with the longest length
+           maxLength =  x[i].length;
+        }
+    }
+    return maxLength;
+}
   
-// console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
+console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
+
+//Add Items Using splice()
+
+function htmlColorNames(arr) {
+    arr.splice(0, 2, 'DarkSalmon', 'BlanchedAlmond', )
+    return arr;
+}
+  
+console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
+
+// Copy Array Items Using slice()
+
+function forecast(arr) {
+    
+    return arr.slice(2, 4);
+  
+}
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+
+
