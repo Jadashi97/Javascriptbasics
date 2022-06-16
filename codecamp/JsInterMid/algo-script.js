09b6ab5bb29799960be32a81453f729a097f3fbf
jadashi97 <njadauniversal23@yahoo.com>
2022-06-15 19:55:52 -0500

    (feat):revise more basic js concepts(methods, arrays, properties)

--- codecamp/JsInterMid/algo-script.js
+++ codecamp/JsInterMid/algo-script.js
@@ -1,14 +1,18 @@
 // Return Largest Numbers in Arrays
-// finish this QA
 
 function largestOfFour(arr) {
-    let maxNum = [];
-
-    for(let i = 0; i < arr[i]; i++){
-        
-        // if(sum(arr[i]) );
+    let results = []; //a variable to store the results array
+    for(let i = 0; i < arr.length; i++){ //outer loop to iterate thru the outer array
+        let largestNumber = arr[i][0]; //variable to hold the second number
+        for(let j = 1; j < arr[i].length; j++){ //inner loop to work with sub arrays
+            if(arr[i][j] > largestNumber){ //check if the element of sub array is larger than currently stored largest number
+                largestNumber = arr[i][j];
+            }
+        }
+        results[i] = largestNumber;
     };
-    // return arr;
-}
-  
+    return results; 
+};
+
+
 console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
--- codecamp/JsInterMid/scope.js
+++ codecamp/JsInterMid/scope.js
@@ -1,76 +1,112 @@
-//compare let and var keys
-function checkScope() {
-    let i = 'function scope';
-    if (true) {
-       let i = 'block scope';
-      console.log('Block scope i is: ', i);
-    }
-    console.log('Function scope i is: ', i);
-    return i;
-};
+// //compare let and var keys
+// function checkScope() {
+//     let i = 'function scope';
+//     if (true) {
+//        let i = 'block scope';
+//       console.log('Block scope i is: ', i);
+//     }
+//     console.log('Function scope i is: ', i);
+//     return i;
+// };
 
-console.log(checkScope());
+// console.log(checkScope());
 
-//mutate an array declared with const
-const s = [5, 7, 2];
-function editInPlace() {
-  // Only change code below this line
-  console.log(s[0] = 2,
-            s[1] = 5,
-                s[2] = 7);
-  // Using s = [2, 5, 7] would be invalid
+// //mutate an array declared with const
+// const s = [5, 7, 2];
+// function editInPlace() {
+//   // Only change code below this line
+//   console.log(s[0] = 2,
+//             s[1] = 5,
+//                 s[2] = 7);
+//   // Using s = [2, 5, 7] would be invalid
 
-  // Only change code above this line
-}
-editInPlace();
+//   // Only change code above this line
+// }
+// editInPlace();
 
-//Prevent object mutation
-function freezeObj() {
-    const MATH_CONSTANTS = {
-      PI: 3.14
-    };
-    // Only change code below this line
-    Object.freeze(MATH_CONSTANTS);
+// //Prevent object mutation
+// function freezeObj() {
+//     const MATH_CONSTANTS = {
+//       PI: 3.14
+//     };
+//     // Only change code below this line
+//     Object.freeze(MATH_CONSTANTS);
   
-    // Only change code above this line
-    try {
-      MATH_CONSTANTS.PI = 99;
-    } catch(ex) {
-      console.log(ex);
-    }
-    return MATH_CONSTANTS.PI;
+//     // Only change code above this line
+//     try {
+//       MATH_CONSTANTS.PI = 99;
+//     } catch(ex) {
+//       console.log(ex);
+//     }
+//     return MATH_CONSTANTS.PI;
+//   }
+//   const PI = freezeObj();
+//   console.log(PI);
+
+// // set default parameters for functions
+
+// // Only change code below this line
+// const increment = (number, value) => {
+//   if(value = " "){
+//     return number++;
+//   }else{
+//     return number + value};
+// };
+
+// console.log(increment(2, ));
+
+
+// // use Rest parameters with function parameters
+
+// const sum = (...args) => { //this allows us to use multiple arguments
+//   return args.reduce((a, b) => a + b, 0);
+// }
+
+// // console.log(sum(2,4,3,5,3));
+
+
+// ////destructuring assignment to extract values
+// //
+// const HIGH_TEMPERATURES = {
+//   yesterday: 75,
+//   today: 77,
+//   tomorrow: 80
+// };
+// //below is a destructured form of the above
+
+// const {today,tomorrow, yesterday} = HIGH_TEMPERATURES;
+
+// // Use Destructuring Assignment to Assign Variables from Nested Objects
+// const LOCAL_FORECAST = {
+//   yesterday: { low: 61, high: 75 },
+//   today: { low: 64, high: 77 },
+//   tomorrow: { low: 68, high: 80 }
+// };
+
+// const {today:{
+//   low: lowToday,
+//   high: highToday
+// }} = LOCAL_FORECAST;
+
+// console.log(highToday);
+
+// // Use Destructuring Assignment to Assign Variables from Arrays
+
+// let a = 8, b = 6;
+// [a, b] = [b, a];
+// console.log(a);
+
+
+// Catch Off By One Errors When Using Indexing
+
+function countToFive() {
+  let firstFive = "12345";
+  let len = firstFive.length;
+  // Only change code below this line
+  for (let i = 0; i < len; i++) {
+  // Only change code above this line
+    console.log(firstFive[i]);
   }
-  const PI = freezeObj();
-  console.log(PI);
-
-// set default parameters for functions
-
-// Only change code below this line
-const increment = (number, value) => {
-  if(value = " "){
-    return number++;
-  }else{
-    return number + value};
-};
-
-console.log(increment(2, ));
-
-
-// use Rest parameters with function parameters
-
-const sum = (...args) => { //this allows us to use multiple arguments
-  return args.reduce((a, b) => a + b, 0);
 }
 
-// console.log(sum(2,4,3,5,3));
-
-
-////destructuring assignment to extract values
-const HIGH_TEMPERATURES = {
-  yesterday: 75,
-  today: 77,
-  tomorrow: 80
-};
-//below is a destructured form of the above
-
-const {today,tomorrow} = HIGH_TEMPERATURES;
+countToFive();
