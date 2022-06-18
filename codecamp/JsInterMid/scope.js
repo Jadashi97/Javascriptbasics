05898b3fa3025cb40208ab00d1744485cb27c041
jadashi97 <njadauniversal23@yahoo.com>
2022-06-17 20:38:38 -0500

    (feat): solve more problems on destructuring to pass objects as functions

--- codecamp/JsInterMid/scope.js
+++ codecamp/JsInterMid/scope.js
@@ -1,100 +1,100 @@
-// //compare let and var keys
-// function checkScope() {
-//     let i = 'function scope';
-//     if (true) {
-//        let i = 'block scope';
-//       console.log('Block scope i is: ', i);
-//     }
-//     console.log('Function scope i is: ', i);
-//     return i;
-// };
+//compare let and var keys
+function checkScope() {
+    let i = 'function scope';
+    if (true) {
+       let i = 'block scope';
+      console.log('Block scope i is: ', i);
+    }
+    console.log('Function scope i is: ', i);
+    return i;
+};
 
-// console.log(checkScope());
+console.log(checkScope());
 
-// //mutate an array declared with const
-// const s = [5, 7, 2];
-// function editInPlace() {
-//   // Only change code below this line
-//   console.log(s[0] = 2,
-//             s[1] = 5,
-//                 s[2] = 7);
-//   // Using s = [2, 5, 7] would be invalid
+//mutate an array declared with const
+const s = [5, 7, 2];
+function editInPlace() {
+  // Only change code below this line
+  console.log(s[0] = 2,
+            s[1] = 5,
+                s[2] = 7);
+  // Using s = [2, 5, 7] would be invalid
 
-//   // Only change code above this line
-// }
-// editInPlace();
+  // Only change code above this line
+}
+editInPlace();
 
-// //Prevent object mutation
-// function freezeObj() {
-//     const MATH_CONSTANTS = {
-//       PI: 3.14
-//     };
-//     // Only change code below this line
-//     Object.freeze(MATH_CONSTANTS);
+//Prevent object mutation
+function freezeObj() {
+    const MATH_CONSTANTS = {
+      PI: 3.14
+    };
+    // Only change code below this line
+    Object.freeze(MATH_CONSTANTS);
   
-//     // Only change code above this line
-//     try {
-//       MATH_CONSTANTS.PI = 99;
-//     } catch(ex) {
-//       console.log(ex);
-//     }
-//     return MATH_CONSTANTS.PI;
-//   }
-//   const PI = freezeObj();
-//   console.log(PI);
+    // Only change code above this line
+    try {
+      MATH_CONSTANTS.PI = 99;
+    } catch(ex) {
+      console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+  }
+  const PI = freezeObj();
+  console.log(PI);
 
-// // set default parameters for functions
+// set default parameters for functions
 
-// // Only change code below this line
-// const increment = (number, value) => {
-//   if(value = " "){
-//     return number++;
-//   }else{
-//     return number + value};
-// };
+// Only change code below this line
+const increment = (number, value) => {
+  if(value = " "){
+    return number++;
+  }else{
+    return number + value};
+};
 
-// console.log(increment(2, ));
-
-
-// // use Rest parameters with function parameters
-
-// const sum = (...args) => { //this allows us to use multiple arguments
-//   return args.reduce((a, b) => a + b, 0);
-// }
-
-// // console.log(sum(2,4,3,5,3));
+console.log(increment(2, ));
 
 
-// ////destructuring assignment to extract values
-// //
-// const HIGH_TEMPERATURES = {
-//   yesterday: 75,
-//   today: 77,
-//   tomorrow: 80
-// };
-// //below is a destructured form of the above
+// use Rest parameters with function parameters
 
-// const {today,tomorrow, yesterday} = HIGH_TEMPERATURES;
+const sum = (...args) => { //this allows us to use multiple arguments
+  return args.reduce((a, b) => a + b, 0);
+}
 
-// // Use Destructuring Assignment to Assign Variables from Nested Objects
-// const LOCAL_FORECAST = {
-//   yesterday: { low: 61, high: 75 },
-//   today: { low: 64, high: 77 },
-//   tomorrow: { low: 68, high: 80 }
-// };
+// console.log(sum(2,4,3,5,3));
 
-// const {today:{
-//   low: lowToday,
-//   high: highToday
-// }} = LOCAL_FORECAST;
 
-// console.log(highToday);
+////destructuring assignment to extract values
+//
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+//below is a destructured form of the above
 
-// // Use Destructuring Assignment to Assign Variables from Arrays
+const {today,tomorrow, yesterday} = HIGH_TEMPERATURES;
 
-// let a = 8, b = 6;
-// [a, b] = [b, a];
-// console.log(a);
+// Use Destructuring Assignment to Assign Variables from Nested Objects
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 }
+};
+
+const {today:{
+  low: lowToday,
+  high: highToday
+}} = LOCAL_FORECAST;
+
+console.log(highToday);
+
+// Use Destructuring Assignment to Assign Variables from Arrays
+
+let a = 8, b = 6;
+[a, b] = [b, a];
+console.log(a);
 
 
 // Catch Off By One Errors When Using Indexing
@@ -110,3 +110,30 @@ function countToFive() {
 }
 
 countToFive();
+
+//destructuring assignments with the Rest parameter to Reassign Array  elements
+
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+  const [a,b,...arr] = list; //use destructuring and spread the Rest parameter values in arr
+
+  return arr;
+}
+const arr = removeFirstTwo(source);
+
+console.log(arr);
+
+// Use Destructuring Assignment to Pass an Object as a Function's Parameters
+
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+const half =({max, min}) => (max + min)/2.0;
+
+console.log(half);
