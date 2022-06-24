8c01f4e0c6d665bb4fc79e7848c42808372f4f46
jadashi97 <njadauniversal23@yahoo.com>
2022-06-23 20:19:03 -0500

    (feat): pass call back functions as a parameter

--- codecamp/JsAdvanced/functionalProgramming.js
+++ codecamp/JsAdvanced/functionalProgramming.js
@@ -1,12 +1,15 @@
 // Function that returns a string representing a cup of green tea
-const prepareTea = () => 'greenTea';
+const prepareGreenTea = () => 'greenTea';
+
+//Function that returns a string representing a cup of green tea
+const prepareBlackTea = () => 'blackTea';
 
 /*
 Given a function (representing the tea type) and number of cups needed, the
 following function returns an array of strings (each representing a cup of
 a specific type of tea).
 */
-const getTea = (numOfCups) => {
+const getTea = (prepareTea, numOfCups) => {
   const teaCups = [];
 
   for(let cups = 1; cups <= numOfCups; cups += 1) {
@@ -16,10 +19,18 @@ const getTea = (numOfCups) => {
   return teaCups;
 };
 
-// Only change code below this line
-const tea4TeamFCC = getTea(40);
-// Only change code above this line
+// const tea4TeamFCC = getTea(40);
 
-console.log(tea4TeamFCC); //this gets the teacups
+// console.log(tea4TeamFCC); //this gets the teacups
 
-//UNDESTANDING FUNCTIONAL PROGRAMMING TERMINOLOGY
+
+// using same above example to Understand Functional Programming Terminology
+
+//we pass the prepareGreenTea and prepareBlackTea as call back functions for the getTea functions
+const tea4GreenTeamFCC = getTea(prepareGreenTea, 27); 
+const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);
+
+console.log(
+  tea4GreenTeamFCC,
+  tea4BlackTeamFCC
+);
