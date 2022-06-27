dfd6e2fb022c320f17372bd2684e339b6fe6f2f0
jadashi97 <njadauniversal23@yahoo.com>
2022-06-27 10:32:35 -0500

    (feat): understand the hazards of having imperative code

--- codecamp/JsAdvanced/functionalProgramming.js
+++ codecamp/JsAdvanced/functionalProgramming.js
@@ -1,36 +1,83 @@
-// Function that returns a string representing a cup of green tea
-const prepareGreenTea = () => 'greenTea';
+// // Function that returns a string representing a cup of green tea
+// const prepareGreenTea = () => 'greenTea';
 
-//Function that returns a string representing a cup of green tea
-const prepareBlackTea = () => 'blackTea';
+// //Function that returns a string representing a cup of green tea
+// const prepareBlackTea = () => 'blackTea';
 
-/*
-Given a function (representing the tea type) and number of cups needed, the
-following function returns an array of strings (each representing a cup of
-a specific type of tea).
-*/
-const getTea = (prepareTea, numOfCups) => {
-  const teaCups = [];
+// /*
+// Given a function (representing the tea type) and number of cups needed, the
+// following function returns an array of strings (each representing a cup of
+// a specific type of tea).
+// */
+// const getTea = (prepareTea, numOfCups) => {
+//   const teaCups = [];
 
-  for(let cups = 1; cups <= numOfCups; cups += 1) {
-    const teaCup = prepareTea();
-    teaCups.push(teaCup);
-  }
-  return teaCups;
+//   for(let cups = 1; cups <= numOfCups; cups += 1) {
+//     const teaCup = prepareTea();
+//     teaCups.push(teaCup);
+//   }
+//   return teaCups;
+// };
+
+// // const tea4TeamFCC = getTea(40);
+
+// // console.log(tea4TeamFCC); //this gets the teacups
+
+
+// // using same above example to Understand Functional Programming Terminology
+
+// //we pass the prepareGreenTea and prepareBlackTea as call back functions for the getTea functions
+// const tea4GreenTeamFCC = getTea(prepareGreenTea, 27); 
+// const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);
+
+// console.log(
+//   tea4GreenTeamFCC,
+//   tea4BlackTeamFCC
+// );
+
+
+// UNDERSTAND THE HAZARDS OF USING IMPERATIVE CODE
+
+// tabs is an array of titles of each site open within the window
+const Window = function(tabs) {
+  this.tabs = tabs; // We keep a record of the array inside the object
 };
 
-// const tea4TeamFCC = getTea(40);
+// When you join two windows into one window
+Window.prototype.join = function(otherWindow) {
+  this.tabs = this.tabs.concat(otherWindow.tabs);
+  return this;
+};
 
-// console.log(tea4TeamFCC); //this gets the teacups
+// When you open a new tab at the end
+Window.prototype.tabOpen = function(tab) {
+  this.tabs.push('new tab'); // Let's open a new tab for now
+  return this;
+};
 
+// When you close a tab
+Window.prototype.tabClose = function(index) {
 
-// using same above example to Understand Functional Programming Terminology
+  // Only change code below this line
 
-//we pass the prepareGreenTea and prepareBlackTea as call back functions for the getTea functions
-const tea4GreenTeamFCC = getTea(prepareGreenTea, 27); 
-const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);
+  const tabsBeforeIndex = this.tabs.splice(0, index); // Get the tabs before the tab
+  const tabsAfterIndex = this.tabs.splice(1); // Get the tabs after the tab
 
-console.log(
-  tea4GreenTeamFCC,
-  tea4BlackTeamFCC
-);
+  this.tabs = tabsBeforeIndex.concat(tabsAfterIndex); // Join them together
+
+  // Only change code above this line
+
+  return this;
+};
+
+// Let's create three browser windows
+const workWindow = new Window(['GMail', 'Inbox', 'Work mail', 'Docs', 'freeCodeCamp']); // Your mailbox, drive, and other work sites
+const socialWindow = new Window(['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium']); // Social sites
+const videoWindow = new Window(['Netflix', 'YouTube', 'Vimeo', 'Vine']); // Entertainment sites
+
+// Now perform the tab opening, closing, and other operations
+const finalTabs = socialWindow
+  .tabOpen() // Open a new tab for cat memes
+  .join(videoWindow.tabClose(2)) // Close third tab in video window, and join
+  .join(workWindow.tabClose(1).tabOpen());
+console.log(finalTabs.tabs);
